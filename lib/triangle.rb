6677b293ce97804293fee51e0d3ef75b858c53c4
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-17 14:16:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
     else
       if (@side1 == @side2) && (@side2 == @side3)
         :equilateral
-      elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side_3)
+      elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
         :isosceles
       elsif (@side1 != @side2) && (@side2 != @side3) && (@side1 != @side3)
         :scalene
